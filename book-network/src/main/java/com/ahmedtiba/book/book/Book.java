29479e63d543f244a550d2636db45679aabc0e7d
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2024-04-23 18:21:03 +0100

    Create Book / Feedback entity and use inheritance and refactor the code

---
+++ book-network/src/main/java/com/ahmedtiba/book/book/Book.java
@@ -0,0 +1,36 @@
+package com.ahmedtiba.book.book;
+
+import com.ahmedtiba.book.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+}
