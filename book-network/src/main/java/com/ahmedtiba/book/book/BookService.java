e75d6ba31678de3027ee6cd401c072dd016c19e5
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2024-04-25 11:58:34 +0100

    Implement the save book method

---
+++ book-network/src/main/java/com/ahmedtiba/book/book/BookService.java
@@ -0,0 +1,21 @@
+package com.ahmedtiba.book.book;
+
+import com.ahmedtiba.book.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookMapper bookMapper;
+    private final BookRepository bookRepository;
+
+    public Integer saveBook(BookRequest request, Authentication connectedUser) {
+        User user = (User) connectedUser.getPrincipal();
+        Book book = bookMapper.toBook(request);
+        book.setOwner(user);
+        return bookRepository.save(book).getId();
+    }
+}
