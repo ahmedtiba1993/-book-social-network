f702fe439addddc82ae63cc66311b5a1fe3c1803
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2024-04-25 12:47:11 +0100

    Implement the functionality to find all displayable books with pagination

--- book-network/src/main/java/com/ahmedtiba/book/book/BookController.java
+++ book-network/src/main/java/com/ahmedtiba/book/book/BookController.java
@@ -1,11 +1,14 @@
 package com.ahmedtiba.book.book;
 
+import com.ahmedtiba.book.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("books")
@@ -23,5 +26,14 @@ public class BookController {
     @GetMapping("/{book-id}")
     public ResponseEntity<BookResponse> getBook(@PathVariable("book-id") Integer bookId) {
         return ResponseEntity.ok(service.findById(bookId));
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
     }
 }
--- book-network/src/main/java/com/ahmedtiba/book/book/BookService.java
+++ book-network/src/main/java/com/ahmedtiba/book/book/BookService.java
@@ -1,10 +1,18 @@
 package com.ahmedtiba.book.book;
 
+import com.ahmedtiba.book.common.PageResponse;
 import com.ahmedtiba.book.user.User;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +32,23 @@ public class BookService {
             return bookRepository.findById(bookId)
                     .map(bookMapper::toBookResponse)
                     .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
+    }
+
+    public PageResponse<BookResponse> findAllBooks(int page, int size, Authentication connectedUser) {
+        User user = (User) connectedUser.getPrincipal();
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
+        Page<Book> books = bookRepository.findAllDisplayableBooks(pageable, user.getId());
+        List<BookResponse> booksResponse = books.stream()
+                .map(bookMapper::toBookResponse)
+                .toList();
+        return new PageResponse<>(
+                booksResponse,
+                books.getNumber(),
+                books.getSize(),
+                books.getTotalElements(),
+                books.getTotalPages(),
+                books.isFirst(),
+                books.isLast()
+        );
     }
 }
