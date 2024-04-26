7c08a0cc37a522af49fa0c59d700b292c219363c
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2024-04-26 23:57:30 +0100

    Implement return borrowed book

--- book-network/src/main/java/com/ahmedtiba/book/book/BookService.java
+++ book-network/src/main/java/com/ahmedtiba/book/book/BookService.java
@@ -106,12 +106,23 @@ public class BookService {
             throw new OperationNotPermittedException("Te requested book is already borrowed");
         }
 
-        BookTransactionHistory bookTransactionHistory = BookTransactionHistory.builder()
-                .user(user)
-                .book(book)
-                .returned(false)
-                .returnApproved(false)
-                .build();
+        BookTransactionHistory bookTransactionHistory = BookTransactionHistory.builder().user(user).book(book).returned(false).returnApproved(false).build();
+        return transactionHistoryRepository.save(bookTransactionHistory).getId();
+    }
+
+    public Integer returnBorrowedBook(Integer bookId, Authentication connectedUser) {
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
+        if (book.isArchived() || !book.isShareable()) {
+            throw new OperationNotPermittedException("The requested book is archived or not shareable");
+        }
+        User user = ((User) connectedUser.getPrincipal());
+        if (Objects.equals(book.getOwner().getId(), user.getId())) {
+            throw new OperationNotPermittedException("You cannot borrow or return your own book");
+        }
+
+        BookTransactionHistory bookTransactionHistory = transactionHistoryRepository.findByBookIdAndUserId(bookId, user.getId()).orElseThrow(() -> new OperationNotPermittedException("You did not borrow this book"));
+
+        bookTransactionHistory.setReturned(true);
         return transactionHistoryRepository.save(bookTransactionHistory).getId();
     }
 }
