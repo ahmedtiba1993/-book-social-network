8b599b678219d27fcb7765043821bb5f110b7c82
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2024-04-26 23:39:36 +0100

    Implement update book archived status

--- book-network/src/main/java/com/ahmedtiba/book/book/BookService.java
+++ book-network/src/main/java/com/ahmedtiba/book/book/BookService.java
@@ -123,4 +123,16 @@ public class BookService {
         bookRepository.save(book);
         return bookId;
     }
+
+    public Integer updateArchivedStatus(Integer bookId, Authentication connectedUser) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
+        User user = ((User) connectedUser.getPrincipal());
+        if (!Objects.equals(book.getOwner().getId(), user.getId())) {
+            throw new OperationNotPermittedException("You cannot update others books archived status");
+        }
+        book.setArchived(!book.isArchived());
+        bookRepository.save(book);
+        return bookId;
+    }
 }
