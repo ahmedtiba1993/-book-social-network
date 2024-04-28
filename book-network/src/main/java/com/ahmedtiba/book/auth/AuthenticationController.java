12d2ed2898065ee16a143ac9a86544c57c8215ef
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2024-04-28 14:09:03 +0100

    Implement the Login page

--- book-network/src/main/java/com/ahmedtiba/book/auth/AuthenticationController.java
+++ book-network/src/main/java/com/ahmedtiba/book/auth/AuthenticationController.java
@@ -24,7 +24,7 @@ public class AuthenticationController {
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody @Valid AuthenticationRequest request) {
         return ResponseEntity.ok(service.authenticate(request));
     }
 
