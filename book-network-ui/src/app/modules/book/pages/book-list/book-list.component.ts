3066a66e968de93d72ee09edfe68811ee5d49ebc
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2024-04-28 23:11:41 +0100

    Inject the jwt token using an HTTP interceptor

--- book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BookService} from "../../../../services/services/book.service";
+import {Router} from "@angular/router";
+import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  bookResponse : PageResponseBookResponse = {};
+  public page = 0;
+  public size = 5;
 
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit() {
+    this.findAllBooks();
+  }
+
+  private findAllBooks() {
+    this.bookService.findAllBooks({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: (books) => {
+        this.bookResponse = books;
+      }
+    })
+  }
 }
