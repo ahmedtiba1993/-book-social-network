d52ccbb6b43e98a3419cfcbd0ff22481080e2b54
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2024-05-02 11:33:07 +0100

    Implement the My book page

---
+++ book-network-ui/src/app/modules/book/pages/my-books/my-books.component.ts
@@ -0,0 +1,83 @@
+import {Component, OnInit} from '@angular/core';
+import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
+import {BookService} from "../../../../services/services/book.service";
+import {Router} from "@angular/router";
+import {BookResponse} from "../../../../services/models/book-response";
+
+@Component({
+  selector: 'app-my-books',
+  templateUrl: './my-books.component.html',
+  styleUrls: ['./my-books.component.scss']
+})
+export class MyBooksComponent implements OnInit{
+  bookResponse : PageResponseBookResponse = {};
+  page = 0;
+  size = 3;
+  pages: any = [];
+
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit() {
+    this.findAllBooks();
+  }
+
+  private findAllBooks() {
+    this.bookService.findAllBooksByOwner({
+      page: this.page,
+      size: this.size
+    })
+      .subscribe({
+        next: (books) => {
+          this.bookResponse = books;
+          /*this.pages = Array(this.bookResponse.totalPages)
+            .fill(0)
+            .map((x, i) => i);*/
+        }
+      });
+  }
+
+  gotToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page --;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  get isLastPage() {
+    return this.page === this.bookResponse.totalPages as number - 1;
+  }
+
+  archiveBook(book: BookResponse) {
+
+  }
+
+  sahreBook(book: BookResponse) {
+
+  }
+
+  editBook(book: BookResponse) {
+
+  }
+}
